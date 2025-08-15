c8a1e761d5adf541806ecf8e8d24a9f048adca09
codewithme112 <154338426+codewithme112@users.noreply.github.com>
2025-08-15 11:24:00 +0530

    jjj

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const ENTRIES_URL = "https://script.google.com/macros/s/AKfycbyXx8XuwuLRaSA0yO2TZxyOnSuK5P8loxv6EhYVvXMn8lk0Uj64hezgx-2CVcyUmWzacA/exec/entries";
+export const SAVE_URL = "https://script.google.com/macros/s/AKfycbyXx8XuwuLRaSA0yO2TZxyOnSuK5P8loxv6EhYVvXMn8lk0Uj64hezgx-2CVcyUmWzacA/exec/save";
+export const TODAY_COUNT_URL = "https://script.google.com/macros/s/AKfycbyXx8XuwuLRaSA0yO2TZxyOnSuK5P8loxv6EhYVvXMn8lk0Uj64hezgx-2CVcyUmWzacA/exec/today-count";
